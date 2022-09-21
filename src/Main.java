6702f2e680ec75fc870bdc4b84d260b64ab457c8
Alexandr <aleksandr.izis@mail.ru>
2022-09-21 20:14:40 +0400

    redid the call of the first file and added the others to the project

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,7 @@ public class Main {
     }
 
     public static void showSecondJavaTask() {
-        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3));
-        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3));
-        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3));
-        ArrayList<String> listWords = new ArrayList<>(Arrays.asList("One", "Two", "Three"));
-
-        CollectionsBlock<Integer> methods = new CollectionsBlock<>();
-//        System.out.println(methods.collectionTask0(list1, list2));
-//        System.out.println(methods.collectionTask1(list3));
-//        System.out.println(methods.collectionTask2(list1, list2));
-//        System.out.println(methods.collectionTask3(list2, 2));
-//        System.out.println(methods.collectionTask4(listWords, "Two", "Second"));
-        methods.collectionTask5();
+        ShowSecondTask.showFirstFileResult();
     }
 
 }
--- src/ShowSecondTask.java
+++ src/ShowSecondTask.java
@@ -1,6 +1,21 @@
 import SecondTask.CollectionsBlock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ShowSecondTask {
     public static void showFirstFileResult(){
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<String> listWords = new ArrayList<>(Arrays.asList("One", "Two", "Three"));
+
+        CollectionsBlock<Integer> methods = new CollectionsBlock<>();
+        System.out.println(methods.collectionTask0(list1, list2));
+        System.out.println(methods.collectionTask1(list3));
+        System.out.println(methods.collectionTask2(list1, list2));
+        System.out.println(methods.collectionTask3(list2, 2));
+        System.out.println(methods.collectionTask4(listWords, "Two", "Second"));
+        methods.collectionTask5();
     }
 }
