d3a25c19eb79c255718d5d487602131cbfceb1be
Alexandr <aleksandr.izis@mail.ru>
2022-09-23 21:41:26 +0400

    completed all tasks

--- src/SecondTask/PracticeTask.java
+++ src/SecondTask/PracticeTask.java
@@ -1,5 +1,8 @@
 package SecondTask;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PracticeTask {
     public interface Printable {
         void print();
@@ -67,14 +70,59 @@ public class PracticeTask {
         }
     }
 
+
+
     public static void repeatTask(int times, Printable task) {
         for (int i = 0; i < times; i++) {
             task.print();
         }
     }
 
-    public static void start() {
+    public static void lambdaTask(){
         Printable myClosure = () -> System.out.println("I love Java");
         PracticeTask.repeatTask(10, myClosure);
+    }
+
+    enum Directions {
+        UP,
+        LEFT,
+        RIGHT,
+        DOWN
+    }
+
+    public static int[] doOneStep(int [] coo, Directions direction){
+        switch (direction){
+            case UP -> coo[1] += 1;
+            case DOWN -> coo[1] -= 1;
+            case LEFT -> coo[0] -= 1;
+            case RIGHT -> coo[0] +=1;
+        }
+        return coo;
+    }
+
+    public static void doManySteps(int[] location, ArrayList<Directions> directionsList){
+        for (int i = 0; i < directionsList.size(); i++){
+            System.out.println(Arrays.toString(doOneStep(location, directionsList.get(i))));
+        }
+    }
+    public static void traveler(){
+        int[] startPosition = new int[] {0,0};
+        ArrayList<Directions> directions = new ArrayList<>();
+        directions.add(Directions.UP);
+        directions.add(Directions.UP);
+        directions.add(Directions.LEFT);
+        directions.add(Directions.DOWN);
+        directions.add(Directions.LEFT);
+        directions.add(Directions.DOWN);
+        directions.add(Directions.DOWN);
+        directions.add(Directions.RIGHT);
+        directions.add(Directions.RIGHT);
+        directions.add(Directions.DOWN);
+        directions.add(Directions.RIGHT);
+        doManySteps(startPosition, directions);
+    }
+
+    public static void start() {
+        PracticeTask.traveler();
     }
 }
